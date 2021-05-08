d231d0618fb79e7ce73ec56ffb2e380702edfff1
Olasupo <olasupoodebiyi@yahoo.com>
2021-05-08 21:02:49 +0100

    Recommit the first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import styled from "styled-components";
 
-function App() {
+const Button = styled.a`
+  height: 100px;
+  width: 203px;
+  background-color: palevioletred;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: gray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Main = styled.div`
+  width: 65vw;
+  background-color: white;
+  height: 100vh;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Main>
+        <div>This is React!</div>;<Button>Buttons!</Button>
+      </Main>
+    </Container>
   );
-}
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
